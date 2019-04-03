6a0cd1e5f90d77540041b894b4c0b7183a83273e
Dirk Hesse <herr.dirk.hesse@gmail.com>
2019-04-03 19:38:29 +0200

    Adding segments notebook and infrastructure.

--- strava.py
+++ strava.py
@@ -1,5 +1,6 @@
 import requests
 import itertools
+from secret import CLIENT_ID, CLIENT_SECRET
 
 def get_activities(page, auth):
     headers = {"Authorization": "Bearer {access_token}".format(**auth)}
@@ -19,3 +20,18 @@ def iter_activities(auth):
     for page in iter_activitiy_pages(auth):
         for activity in page:
             yield activity
+
+def refresh_token(auth):
+    token_url = 'https://www.strava.com/oauth/token'
+    response = requests.post(token_url, {'client_id': CLIENT_ID,
+                                         'client_secret': CLIENT_SECRET,
+                                         'refresh_token': auth['refresh_token'],
+                                         'grant_type': 'refresh_token'})
+    return response.json()
+
+def get_activity(id, auth):
+    headers = {"Authorization": "Bearer {access_token}".format(**auth)}
+    url = 'https://www.strava.com/api/v3/activities/{id}'.format(id=id)
+    return requests.get(url,
+                        params={'include_all_efforts': True},
+                        headers=headers).json()
